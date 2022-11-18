45a6039fc047341c5944de732fe09ceed456af2c
DA M1 61 <DAM161@DAs-MacBook-48.local>
2022-11-18 08:14:06 +0200

    added south africa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>Hello Sithembiso</div>
-  )
-}
+    <div className="app">
+      <img
+        src="https://images.unsplash.com/photo-1613244600331-ac72a1ee5256?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+        alt="south"
+      ></img>
+    </div>
+  );
+};
 
-export default App
+export default App;
